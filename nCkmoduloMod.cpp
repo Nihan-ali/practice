c919e2a28e65a250410a3ada71e5b2bb3eea40cb
Nihan-ali <83661275+Nihan-ali@users.noreply.github.com>
2022-02-15 10:32:31 +0600

    Update and rename nCkmoduloMod to nCkmoduloMod.cpp

--- nCkmoduloMod.cpp
+++ nCkmoduloMod.cpp
@@ -1,3 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
 int mod = 1e9 + 7;
 int fast_pow(int a, int p) {
   int res = 1;
@@ -23,4 +27,11 @@ int fact(int n) {
 
 int nCk(int n, int k) {
   return fact(n) * 1ll * fast_pow(fact(k), mod - 2) % mod * 1ll * fast_pow(fact(n - k), mod - 2) % mod;
+}
+int main()
+{
+  int n,k,mod;
+  cin>>n>>k>>mod;
+  cout<<nCk(n,k)<<endl;
+  return 0;
 }
