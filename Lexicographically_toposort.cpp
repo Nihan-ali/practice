b8f1494c4aebe3805519bbf2f41a4ccbc3a52003
Nihan-ali <83661275+Nihan-ali@users.noreply.github.com>
2022-03-15 00:45:51 +0600

    Lexicographically Toposort

---
+++ Lexicographically_toposort.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define N 1000
+int indeg[N+5]= {0};
+vector<int>adj[N+5];
+vector<int>ans;
+
+void LexicographicallyTopoSort(int n)
+{
+    priority_queue<int,vector<int>,greater<int>>q;
+    for(int i=0;i<n;i++)
+    {
+        if(!indeg[i]) q.push(i);
+    }
+    while(!q.empty())
+    {
+        int u=q.top();
+        ans.push_back(u);
+        q.pop();
+        for(auto v:adj[u])
+        {
+            if(--indeg[v]==0) q.push(v);
+        }
+    }
+    if(ans.size()!=n) 
+    {
+        cout<<"No TopoSort exits\n";
+        return;
+    }
+    for(auto i:ans)
+    {
+        cout<<i<<' ';
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    int n,m,u,v;
+    cin>>n>>m;
+
+    while(m--)
+    {
+        cin>>u>>v;
+        adj[u].push_back(v);
+        ++indeg[v];
+    }
+    LexicographicallyTopoSort(n);
+    return 0;
+}
+
+
